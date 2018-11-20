eff588b3c2583c92d01054cdf3f8da1fd0820c3c
NathanB09 <nathanbell09@gmail.com>
2018-11-20 17:45:52 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,6 +2,16 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+# iterate over the response hash to find the collection of `films` for the given
+#   `character`
+# collect those film API urls, make a web request to each URL to get the info
+#  for that film
+# return value of this method should be collection of info about each film.
+#  i.e. an array of hashes in which each hash reps a given film
+# this collection will be the argument given to `print_movies`
+#  and that method will do some nice presentation stuff like puts out a list
+#  of movies by title. Have a play around with the puts with other info about a given film.
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_hash = to_json('http://www.swapi.co/api/people/')
@@ -14,15 +24,6 @@ def get_character_movies_from_api(character_name)
     end
   end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
 def print_movies(films)
@@ -32,6 +33,10 @@ def print_movies(films)
     counter += 1
     puts "#{counter}. #{film["title"]}"
   end
+  # Better:
+  # films.each_with_index do |film, index|
+  #   p "#{index + 1}. #{film["title"]}"
+  # end
 end
 
 def show_character_movies(character)
