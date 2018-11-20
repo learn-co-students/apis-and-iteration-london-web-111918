91dfacc74ec534f95855e55d29cbd2ebdb689fec
jodyneckles <jody@technative.tv>
2018-11-20 18:47:21 +0000

    Modified CLI and API files and added starwars.txt

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,24 +1,11 @@
 def welcome
   # puts out a welcome message here!
-  puts "      ________________.  ___     .______
-     /                | /   \    |   _  \
-    |   (-----|  |----`/  ^  \   |  |_)  |
-     \   \    |  |    /  /_\  \  |      /
-.-----)   |   |  |   /  _____  \ |  |\  \-------.
-|________/    |__|  /__/     \__\| _| `.________|
- ____    __    ____  ___     .______    ________.
- \   \  /  \  /   / /   \    |   _  \  /        |
-  \   \/    \/   / /  ^  \   |  |_)  ||   (-----`
-   \            / /  /_\  \  |      /  \   \
-    \    /\    / /  _____  \ |  |\  \---)   |
-     \__/  \__/ /__/     \__\|__| `._______/
-
-------------------------------------------------"
-puts "please type your Star Wars character name"
+  banner = File.readlines('starwars.txt')
+  puts banner
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
-  gets.chomp.downcased
+  gets.chomp.downcase
 end
