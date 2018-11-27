dadfb122278538e91a3adb9660a0a1f7c34df540
IMH51 <ianmartinharrison@outlook.com>
2018-11-27 13:33:23 +0000

    changed variable name in run file

--- bin/run.rb
+++ bin/run.rb
@@ -5,14 +5,14 @@ require_relative "../lib/command_line_interface.rb"
 require_relative "../lib/film_api_communicator.rb"
 
 welcome
-choice = character_or_film
-until choice == "c" || choice == "f" do
-  choice = character_or_film
+key_choice = character_or_film
+until key_choice == "c" || key_choice == "f" do
+  key_choice = character_or_film
 end
-if choice == "c"
+if key_choice == "c"
   character = get_character_from_user
   show_character_movies(character)
-elsif choice == "f"
+elsif key_choice == "f"
   film = get_film_from_user
   show_film_details(film)
 end
